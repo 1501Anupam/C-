a2f618af4bb217049eedd4adfa4df29328edff4f
Anupam <43044405+1501Anupam@users.noreply.github.com>
2021-06-15 01:57:56 +0530

    Added advanced patterns.

---
+++ AdvancedPatterns/decreasing_number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row, j;
+    cin >> row;
+    for (int i = 1; i <= row; i++)
+    {
+        for (j = 1; j <= (row - i); j++)
+        {
+            cout << " ";
+        }
+        int k = i;
+        for (; j <= row; j++)
+        {
+            cout << k-- << " ";
+        }
+        k = 2;
+        for (; j <= row + i - 1; j++)
+        {
+            cout << k++ << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
